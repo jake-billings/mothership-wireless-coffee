1f2b0989db369c5feab2d210e6de22319b89d909
jakebillings <jake@eakjb.com>
2016-11-12 17:22:10 -0700

    Admin URL

--- app/components/Root/Root.component.js
+++ app/components/Root/Root.component.js
@@ -2,6 +2,9 @@
 angular.module('mothership-wireless-coffee')
     .directive('mothershipWirelessCoffee', function () {
         return {
-            templateUrl: 'components/Root/Root.html'
+            templateUrl: 'components/Root/Root.html',
+            controller: ['$scope', '$location', function ($scope, $location) {
+                $scope.admin = $location.search().admin;
+            }]
         };
     });
